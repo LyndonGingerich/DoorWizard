41bfc277d1a7b388729ea1f819e9292ce10bffd7
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-25 17:38:46 -0500

    Correct spelling of "unknown"

--- Woz.FSharpRoguelike/Commands.fs
+++ Woz.FSharpRoguelike/Commands.fs
@@ -17,7 +17,7 @@ let private buildCommand validator operation direction actorId =
     let action = operation direction actorId
     composeCommand test action
 
-let invalidCommand = Invalid "Unknow command"
+let invalidCommand = Invalid "Unknown command"
 
 let idleCommand (level: level) = Valid level
 
