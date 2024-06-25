445c75be321027a8c110f036c6b124c06bdf636a
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-25 18:20:33 -0500

    Inline Vector.create

--- Woz.FSharpRoguelike/vector.fs
+++ Woz.FSharpRoguelike/vector.fs
@@ -4,16 +4,14 @@ type Vector =
     { x: int
       y: int }
 
-    static member create x y = { x = x; y = y }
-
     static member op_Addition(lhs, rhs) =
-        Vector.create (lhs.x + rhs.x) (lhs.y + rhs.y)
+        { x = lhs.x + rhs.x; y = lhs.y + rhs.y }
 
     static member op_Subtraction(lhs, rhs) =
-        Vector.create (lhs.x - rhs.x) (lhs.y - rhs.y)
+        { x = lhs.x - rhs.x; y = lhs.y - rhs.y }
 
     static member op_Multiply(lhs: Vector, scale: int) =
-        Vector.create (lhs.x * scale) (lhs.y * scale)
+        { x = lhs.x * scale; y = lhs.y * scale }
 
     static member op_LessThanOrEqual(lhs, rhs) = lhs.x <= rhs.x && lhs.y <= rhs.y
 
@@ -25,8 +23,10 @@ type Vector =
 
     static member op_GreaterThan(lhs, rhs) = lhs.x > rhs.x && lhs.y > rhs.y
 
+let create x y = { x = x; y = y }
+
 let abs location =
-    Vector.create (abs location.x) (abs location.y)
+    create (abs location.x) (abs location.y)
 
 let distanceFrom target location =
     let diff = abs (target - location)
@@ -35,12 +35,12 @@ let distanceFrom target location =
     sqrt (float distanceSq)
 
 module Directions =
-    let idle = Vector.create 0 0
-    let north = Vector.create 0 1
-    let northEast = Vector.create 1 1
-    let east = Vector.create 1 0
-    let southEast = Vector.create 1 (-1)
-    let south = Vector.create 0 -1
-    let southWest = Vector.create (-1) (-1)
-    let west = Vector.create (-1) 0
-    let northWest = Vector.create -1 1
+    let idle = create 0 0
+    let north = create 0 1
+    let northEast = create 1 1
+    let east = create 1 0
+    let southEast = create 1 (-1)
+    let south = create 0 -1
+    let southWest = create (-1) (-1)
+    let west = create (-1) 0
+    let northWest = create -1 1
