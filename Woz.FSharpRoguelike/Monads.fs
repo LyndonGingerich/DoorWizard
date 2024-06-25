1ce75aa68e14f93140b2068ce396cff2232e941a
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-25 18:40:20 -0500

    Remove unused resultOrElse computation expression

--- Woz.FSharpRoguelike/Monads.fs
+++ Woz.FSharpRoguelike/Monads.fs
@@ -10,15 +10,4 @@ module Result =
         member this.Bind(monad, func) = bind func monad
         member this.Return(value) = Ok value
 
-    type resultOrElseFactory() =
-        member this.ReturnFrom(x) = x
-
-        member this.Combine(firstMonad, secondMonad) =
-            match firstMonad with
-            | Ok _ -> firstMonad
-            | Error _ -> secondMonad
-
-        member this.Delay(f) = f ()
-
     let result = resultFactory ()
-    let resultOrElse = resultOrElseFactory ()
