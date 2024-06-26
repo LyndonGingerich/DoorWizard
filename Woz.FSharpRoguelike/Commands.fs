c2f2f85d7fe2b590f381dceb0dd949876e345b10
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-26 17:59:23 -0500

    Add type annotations in Commands

--- Woz.FSharpRoguelike/Commands.fs
+++ Woz.FSharpRoguelike/Commands.fs
@@ -6,13 +6,18 @@ open Operations
 open Result
 open Validation
 
-let private composeCommand validation operation level =
+let private composeCommand (validation: Level -> Result<Level, string>) (operation: Level -> Level) level =
     result {
         let! validLevel = validation level
         return operation validLevel
     }
 
-let private buildCommand validator operation direction actorId =
+let private buildCommand
+    (validator: Vector -> int -> Level -> Result<Level, string>)
+    (operation: Vector -> int -> Level -> Level)
+    direction
+    actorId
+    =
     let test = validator direction actorId
     let action = operation direction actorId
     composeCommand test action
