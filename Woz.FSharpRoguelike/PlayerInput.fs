6bbc1db1a526a146b67e4755456565cd689a2aae
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-26 19:31:05 -0500

    Implement multiple message printing

--- Woz.FSharpRoguelike/PlayerInput.fs
+++ Woz.FSharpRoguelike/PlayerInput.fs
@@ -23,27 +23,33 @@ let private selectActorCommand direction actorId level =
     { result2 with
         Messages = result1.Messages @ result2.Messages }
 
-let rec handleKeyPress activeBuilder actorId =
+let rec handleKeyPress activeBuilder actorId level =
     let workingBuilder =
         match activeBuilder with
         | Some builder -> builder
         | None -> selectActorCommand
 
-    match Console.ReadKey().Key with
-    | ConsoleKey.D5 -> handleKeyPress (Some buildOpenDoorCommand) actorId
-    | ConsoleKey.C -> handleKeyPress (Some buildCloseDoorCommand) actorId
-    | ConsoleKey.U -> handleKeyPress (Some buildUnlockDoorCommand) actorId
-    | ConsoleKey.T -> handleKeyPress (Some buildTakeItemsCommand) actorId
-    | ConsoleKey.D9 -> workingBuilder northEast actorId
-    | ConsoleKey.D8 -> workingBuilder north actorId
-    | ConsoleKey.D7 -> workingBuilder northWest actorId
-    | ConsoleKey.D6 -> workingBuilder east actorId
-    | ConsoleKey.D4 -> workingBuilder west actorId
-    | ConsoleKey.D3 -> workingBuilder southEast actorId
-    | ConsoleKey.D2 -> workingBuilder south actorId
-    | ConsoleKey.D1 -> workingBuilder southWest actorId
-    | ConsoleKey.OemPeriod -> idleCommand
-    | _ -> invalidCommand
+    let inputKey = Console.ReadKey().Key
+
+    if level.Messages.IsEmpty then
+        match inputKey with
+        | ConsoleKey.D5 -> handleKeyPress (Some buildOpenDoorCommand) actorId
+        | ConsoleKey.C -> handleKeyPress (Some buildCloseDoorCommand) actorId
+        | ConsoleKey.U -> handleKeyPress (Some buildUnlockDoorCommand) actorId
+        | ConsoleKey.T -> handleKeyPress (Some buildTakeItemsCommand) actorId
+        | ConsoleKey.D9 -> workingBuilder northEast actorId
+        | ConsoleKey.D8 -> workingBuilder north actorId
+        | ConsoleKey.D7 -> workingBuilder northWest actorId
+        | ConsoleKey.D6 -> workingBuilder east actorId
+        | ConsoleKey.D4 -> workingBuilder west actorId
+        | ConsoleKey.D3 -> workingBuilder southEast actorId
+        | ConsoleKey.D2 -> workingBuilder south actorId
+        | ConsoleKey.D1 -> workingBuilder southWest actorId
+        | ConsoleKey.OemPeriod -> idleCommand
+        | _ -> invalidCommand
+        <| level
+    else
+        OperationResult.success level
 
 let getCommandForActor = handleKeyPress None
 
