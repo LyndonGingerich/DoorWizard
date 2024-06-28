3fd126b1e991cc075f17ad89a7de5b7013d1f59a
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-28 18:25:13 -0500

    Add module DieRoll

---
+++ Woz.FSharpRoguelike/DieRoll.fs
@@ -0,0 +1,10 @@
+﻿[<RequireQualifiedAccess>]
+module DieRoll
+
+open System
+
+let private next maxValue (random: Random) = random.Next(1, maxValue)
+
+let d count size () =
+    let random = Random()
+    next (size + 1) |> List.replicate count |> List.sumBy (fun f -> f random)
