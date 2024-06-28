041a7da22adbbb0463c207ac5badb841a435d004
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-27 19:13:46 -0500

    Remove AI stubs

--- Woz.FSharpRoguelike/GameLoop.fs
+++ Woz.FSharpRoguelike/GameLoop.fs
@@ -1,34 +1,13 @@
 ﻿module GameLoop
 
-open Commands
-open GameTypes
 open Library
 open PlayerInput
 open Operations
 open Queries.Level
 open RenderEngine
 
-// Stub
-let private getAiCommand actorId = idleCommand
-
-let private runAiCommand level command =
-    (command level).Contents |> Option.defaultValue level
-
-let private runAi (level: Level) =
-    level
-    |> npcIds
-    |> Seq.map getAiCommand
-    |> Seq.fold runAiCommand level
-    |> OperationResult.success
-
 let private runTurn playerCommand level =
-    let turnResult =
-        result {
-            let! playerMoved = level |> playerCommand
-            let! aiMoved = playerMoved |> runAi
-            return aiMoved
-        }
-
+    let turnResult = level |> playerCommand
     turnResult.Contents |> Option.defaultValue level |> logAll turnResult.Messages
 
 let rec gameLoop level =
