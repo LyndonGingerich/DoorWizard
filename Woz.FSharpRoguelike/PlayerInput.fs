2ef54bb6ecb9ee05fe0ea360b34743b6febe74ec
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-27 19:36:32 -0500

    Extract getMaybeDirection from handleKeyPress

--- Woz.FSharpRoguelike/PlayerInput.fs
+++ Woz.FSharpRoguelike/PlayerInput.fs
@@ -10,31 +10,38 @@ open Operations
 open Validation
 open Vector.Directions
 
+let getMaybeDirection =
+    function
+    | ConsoleKey.D9 -> northEast |> Some
+    | ConsoleKey.D8 -> north |> Some
+    | ConsoleKey.D7 -> northWest |> Some
+    | ConsoleKey.D6 -> east |> Some
+    | ConsoleKey.D4 -> west |> Some
+    | ConsoleKey.D3 -> southEast |> Some
+    | ConsoleKey.D2 -> south |> Some
+    | ConsoleKey.D1 -> southWest |> Some
+    | _ -> None
+
 let rec handleKeyPress activeBuilder level =
     let workingBuilder = activeBuilder |> Option.defaultValue movePlayer
 
     let inputKey = Console.ReadKey().Key
 
     if level.Messages.IsEmpty then
-        match inputKey with
-        | ConsoleKey.O -> handleKeyPress (Some(buildCommand canOpenDoor openDoor))
-        | ConsoleKey.C -> handleKeyPress (Some(buildCommand canCloseDoor closeDoor))
-        | ConsoleKey.U -> handleKeyPress (Some(buildCommand canUnlockDoor unlockDoor))
-        | ConsoleKey.T -> handleKeyPress (Some(buildCommand canTakeItems takeItems))
-        | ConsoleKey.OemMinus -> handleKeyPress (Some doorBlastCommand)
-        | ConsoleKey.OemPeriod -> handleKeyPress (Some doorStopperCommand)
-        | ConsoleKey.OemPlus -> handleKeyPress (Some doorBoltCommand)
-        | ConsoleKey.D5 -> handleKeyPress (Some doorBeamCommand)
-        | ConsoleKey.D9 -> workingBuilder northEast
-        | ConsoleKey.D8 -> workingBuilder north
-        | ConsoleKey.D7 -> workingBuilder northWest
-        | ConsoleKey.D6 -> workingBuilder east
-        | ConsoleKey.D4 -> workingBuilder west
-        | ConsoleKey.D3 -> workingBuilder southEast
-        | ConsoleKey.D2 -> workingBuilder south
-        | ConsoleKey.D1 -> workingBuilder southWest
-        | _ -> invalidCommand
-        <| level
+        match getMaybeDirection inputKey with
+        | Some direction -> workingBuilder direction level
+        | None ->
+            match inputKey with
+            | ConsoleKey.O -> handleKeyPress (Some(buildCommand canOpenDoor openDoor))
+            | ConsoleKey.C -> handleKeyPress (Some(buildCommand canCloseDoor closeDoor))
+            | ConsoleKey.U -> handleKeyPress (Some(buildCommand canUnlockDoor unlockDoor))
+            | ConsoleKey.T -> handleKeyPress (Some(buildCommand canTakeItems takeItems))
+            | ConsoleKey.OemMinus -> handleKeyPress (Some doorBlastCommand)
+            | ConsoleKey.OemPeriod -> handleKeyPress (Some doorStopperCommand)
+            | ConsoleKey.OemPlus -> handleKeyPress (Some doorBoltCommand)
+            | ConsoleKey.D5 -> handleKeyPress (Some doorBeamCommand)
+            | _ -> invalidCommand
+            <| level
     else
         OperationResult.success level
 
