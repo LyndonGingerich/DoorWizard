bab50f33261672f4abe4e61a00ac7fcdf511162e
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-28 19:12:53 -0500

    Move GameLoop contents to Program

--- Woz.FSharpRoguelike/Program.fs
+++ Woz.FSharpRoguelike/Program.fs
@@ -2,7 +2,34 @@
 
 open System
 
+let printAll strings =
+    strings |> Seq.iter (printfn "%s")
+    printfn ""
+
+let print string = printfn $"%s{string}\n"
+
+let render level =
+    Console.Clear()
+
+    printAll (RenderEngine.buildLevel level)
+
+    let level, maybeMessage = RenderEngine.getMaybeNextMessage level
+    Option.iter print maybeMessage
+
+    level
+
+let rec gameLoop level =
+    let turnLevel =
+        level
+        |> render
+        |> PlayerInput.runTurn (PlayerInput.handleKeyPress (Console.ReadKey().Key))
+
+    if turnLevel |> Queries.isPlayerDead then
+        print PlayerInput.deathMessage
+    else
+        turnLevel |> gameLoop
+
 [<EntryPoint>]
 let main argv =
-    LevelFactory.testLevel (Random()) |> GameLoop.gameLoop
+    LevelFactory.testLevel (Random()) |> gameLoop
     0 // return an integer exit code
