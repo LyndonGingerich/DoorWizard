679ba47b44be27711fd03740a4056b0e87fd489c
Lyndon Gingerich <gingerichlyndon@gmail.com>
2024-06-28 19:15:31 -0500

    Redesign the randomness module to take a Random instance

---
+++ Woz.FSharpRoguelike/Die.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module Die
+
+open System
+
+let roll random count size () =
+    let next (random: Random) = random.Next(1, size + 1)
+    next |> List.replicate count |> List.sumBy (fun f -> f random)
